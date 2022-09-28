e976f8a209296711181b282c097c83a6f7bfc54b
DucAnh <ducanh.le1908@gmail.com>
2022-09-28 14:11:57 +0700

    add project server

---
+++ index.js
@@ -0,0 +1,24 @@
+const bodyParser = require('body-parser');
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const cors = require('cors')
+const app = express();
+
+
+const DB = process.env.MONGODB_URL
+
+mongoose.connect(DB).then(()=> {
+    console.log('DB connect success')
+})
+.catch((e)=>{
+    console.log(e)
+})
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log("Server is running port", process.env.PORT)
+})
